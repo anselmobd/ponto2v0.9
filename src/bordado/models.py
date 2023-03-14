fba218af5a59ca0e270ab1258505beaaecb7be2d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-03-13 23:01:24 -0300

    criado modelo mínimo para cliente

--- src/bordado/models.py
+++ src/bordado/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Empresa(models.Model):
+    nome = models.CharField(
+        max_length=50,
+    )
+
+    def __str__(self):
+        return f'{self.nome}'
+
+    class Meta:
+        db_table = "po2_empresa"
+        verbose_name = "Empresa"
