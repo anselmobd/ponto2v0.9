b609341a60f3bb38f97a46a464fe6813fda37a32
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-03-14 21:53:34 -0300

    EmpresaAdmin search_fields

--- src/bordado/admin.py
+++ src/bordado/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Empresa)
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    search_fields = ["nome"]
+
+
 admin.site.register(Cliente)
