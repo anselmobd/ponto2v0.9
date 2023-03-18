f01ffcfd42a4da386553a02a4ae88a5289c17bd1
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-03-17 21:28:20 -0300

    alterando o banco de desenvolvimento para postgresql

--- src/ponto2/settings.py
+++ src/ponto2/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'ponto2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': mysecrets.DB_DEV_ENGINE,
+        'NAME': mysecrets.DB_DEV_NAME,
+        'USER': mysecrets.DB_DEV_USER,
+        'PASSWORD': mysecrets.DB_DEV_PASSWORD,
+        'HOST': mysecrets.DB_DEV_HOST,
+        'PORT': mysecrets.DB_DEV_PORT,
     }
 }
 
