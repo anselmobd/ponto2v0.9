06de4d05a38ff23dc56a31a28b4127c539433ad0
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-03-21 17:53:54 -0300

    ALLOWED_HOSTS

--- src/ponto2/settings.py
+++ src/ponto2/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = mysecrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'intra.t',
+]
 
 
 # Application definition
