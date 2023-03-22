7e02d773a92f03aaa3fdad4fac9480a32417e092
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-03-22 10:56:51 -0300

    BordadoAdmin

--- src/bordado/admin.py
+++ src/bordado/admin.py
@@ -5,11 +5,18 @@ from .models import *
 
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
-    search_fields = ["nome"]
+    search_fields = ['nome']
 
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
-    list_display = ["empresa", "nome"]
-    list_display_links = ["nome"]
-    search_fields = ["empresa", "nome"]
+    list_display = ['empresa', 'nome']
+    list_display_links = ['nome']
+    search_fields = ['empresa', 'nome']
+
+
+@admin.register(Bordado)
+class BordadoAdmin(admin.ModelAdmin):
+    list_display = ['cliente', 'nome']
+    list_display_links = ['nome']
+    search_fields = ['cliente', 'nome']
