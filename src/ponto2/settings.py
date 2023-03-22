8315a98190a691b1f66333ecf5fce9b35a9e02ce
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-03-22 16:10:59 -0300

    settings não precisa import os

--- src/ponto2/settings.py
+++ src/ponto2/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 from . import settings_secrets as ss
@@ -68,7 +67,7 @@ ROOT_URLCONF = 'ponto2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "template"), ],
+        'DIRS': [BASE_DIR / "template", ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
