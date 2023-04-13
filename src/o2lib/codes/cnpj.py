fc20b66e5ec1694a5db1aa966e73099ccb5d72fd
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-04-13 14:15:26 -0300

    class CNPJ adicionada inicialização e __str__

--- src/o2lib/codes/cnpj.py
+++ src/o2lib/codes/cnpj.py
@@ -4,8 +4,22 @@ from random import randint
 class CNPJ():
     """Cadastro Nacional da Pessoa Jurídica (CNPJ)"""
 
-    def __init__(self):
-        self.weights = list(range(2, 10)) + list(range(2, 10))
+    weights = list(range(2, 10)) + list(range(2, 10))
+
+    def __init__(self, cnpj9=None, cnpj4=None, cnpj2=None):
+        self.cnpj9 = cnpj9
+        self.cnpj4 = cnpj4
+        self.cnpj2 = cnpj2
+
+        if cnpj9 is not None:
+            if isinstance(cnpj9, str):
+                self.cnpj = cnpj9
+                self.cnpj9 = None
+            else:
+                self.cnpj = self.to_str()
+
+    def __str__(self):
+        return self.to_str()
 
     def _only_digits(self, cnpj):
         return "".join([d for d in cnpj if d.isdigit()])
@@ -19,7 +33,8 @@ class CNPJ():
         sum = 0 if sum < 2 else 11 - sum
         return str(sum)
 
-    def valid(self, cnpj):
+    def valid(self, cnpj=None):
+        cnpj = cnpj if cnpj else self.cnpj
         cnpj = self._only_digits(cnpj)
         if len(cnpj) != 14:
             return False
@@ -43,13 +58,21 @@ class CNPJ():
             cnpjs.add(self.generate())
         return list(cnpjs)
 
-    def to_str(self, cnpj9, cnpj4, cnpj2):
+    def to_str(self, cnpj9=None, cnpj4=None, cnpj2=None):
+        if cnpj9 is None:
+            cnpj9 = self.cnpj9
+            cnpj4 = self.cnpj4
+            cnpj2 = self.cnpj2
         return f'{cnpj9:08d}/{cnpj4:04d}-{cnpj2:02d}'
 
-    def str_mask(self, cnpj):
+    def str_mask(self, cnpj=None):
+        if cnpj is None:
+            cnpj = self.cnpj
         cnpj = self._only_digits(cnpj)
         return f"{cnpj[:8]}/{cnpj[8:12]}-{cnpj[-2:]}"
 
-    def str_mask_dot(self, cnpj):
+    def str_mask_dot(self, cnpj=None):
+        if cnpj is None:
+            cnpj = self.cnpj
         cnpj = self._only_digits(cnpj)
         return f"{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[-2:]}"
