e9d91c972e6971cbccb53d28c610789c0ce30515
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-09-10 18:27:45 -0300

    teste acessando API

--- src/static/bordado/index/main.js
+++ src/static/bordado/index/main.js
@@ -1,11 +1,28 @@
+import axios from 'axios'
+
 export default {
   template:
     /*html*/
     `
-    bordados
+    <h3>Clientes</h3>
+    <ul>
+      <li  v-for="cliente in clientes" :key="cliente.id">
+        {{ cliente.apelido }}
+      </li>
+    </ul>
     `,
   data() {
     return {
+      clientes: {}
     }
+  },
+  mounted() {
+    axios.get('/bordado/api/clientes/?format=json')
+    .then(response => {
+      this.clientes = response.data;
+    })
+    .catch(error => {
+      console.error('Erro ao obter clientes via API:', error);
+    });
   }
 }
