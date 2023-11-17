43c250dfec7bd49f64b43fef658e32bb854d502e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-16 21:50:45 -0300

    front vue.js; only logo

---
+++ front/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" src="./assets/logo.png" width="194" height="194" />
+  </header>
+  <main>
+  </main>
+</template>
+
+<style scoped>
+</style>
