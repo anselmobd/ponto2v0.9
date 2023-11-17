4bc2cabe35731ab75e712fb04421db4d1ad3664e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-16 22:27:58 -0300

    ajustando base tailwind

--- front/src/App.vue
+++ front/src/App.vue
@@ -4,6 +4,7 @@
 <template>
   <header>
     <img alt="Vue logo" src="./assets/logo.png" width="194" height="194" />
+    <div class="text-green-700 text-xl">Ponto</div>
   </header>
   <main>
   </main>
