b8b3994a1c4214643cb7983aee70a6c989c9fc46
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-17 17:22:36 -0300

    criado componente AppFooter

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
+import AppFooter from './components/AppFooter.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" src="./assets/logo.png" width="194" height="194" />
-    <div class="text-green-700 text-xl">Ponto</div>
   </header>
   <main>
   </main>
+  <app-footer />
 </template>
 
 <style scoped>
---
+++ front/src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const thisYear = new Date().getFullYear()
+const year = ref(thisYear)
+</script>
+
+<template>
+  <footer class="sticky bottom-0 text-center border-t border-solid border-gray-800 shadow-[0_-6px_5px_-5px] shadow-gray-800">
+    <span class="whitespace-nowrap">Copyright &copy; {{ year }}</span>&ThinSpace;
+    <span class="whitespace-nowrap">RPR & Oxigenai</span>
+  </footer>
+</template>
+
+<style scoped>
+</style>
