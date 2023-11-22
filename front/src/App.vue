49be8d23c2fcf7db0a8ebdd51689e1697cd97c5a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-22 18:06:38 -0300

    criando components/AppNav.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
 import AppFooter from './components/AppFooter.vue'
+import AppNav from './components/AppNav.vue'
 </script>
 
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'Bordado' }">Bordado</router-link> |
-      <router-link :to="{ name: 'Sobre' }">Sobre</router-link>
-    </div>
-    <router-view class="main-body"/>
-  </div>
+  <app-nav />
+  <router-view class="main-body"/>
   <app-footer />
 </template>
-
-<style scoped>
-</style>
---
+++ front/src/components/AppNav.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div id="nav">
+    <router-link :to="{ name: 'Bordado' }">Bordado</router-link> |
+    <router-link :to="{ name: 'Sobre' }">Sobre</router-link>
+  </div>
+</template>
