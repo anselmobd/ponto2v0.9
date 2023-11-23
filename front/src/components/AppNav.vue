c3741774c41db1969d62578c11a63bd866e9db33
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-22 23:57:34 -0300

    evoluindo app-nav (header)

--- front/src/components/AppNav.vue
+++ front/src/components/AppNav.vue
@@ -2,8 +2,15 @@
 </script>
 
 <template>
-  <div id="nav">
-    <router-link :to="{ name: 'Bordado' }">Bordado</router-link> |
-    <router-link :to="{ name: 'Sobre' }">Sobre</router-link>
+  <div id="nav" class="flex row items-center justify-between border-b border-solid border-slate-800 shadow-[0_3px_3px_-1px] shadow-slate-700">
+    <h1 class="inline text-2xl font-bold">RPR - Ponto2</h1>
+    <div>
+      <router-link :to="{ name: 'Bordado' }">Bordado</router-link> |
+      <router-link :to="{ name: 'Sobre' }">Sobre</router-link>
+    </div>
+    <div>
+      admin
+      <button type="button" class="px-2 border border-solid border-slate-800 rounded-lg bg-blue-600 font-bold text-slate-100">Encerrar</button>
+    </div>
   </div>
 </template>
