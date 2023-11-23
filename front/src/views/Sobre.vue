661bd87a9f28a59a5c47eb8598fb0cc3f8820a55
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-23 00:39:02 -0300

    Sobre com conteúdo

--- front/src/views/Sobre.vue
+++ front/src/views/Sobre.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <h1>Sobre</h1>
+    <article class="m-4">
+      <h3 class="text-xl my-4 font-bold">RPR Bordados Computadorizados</h3>
+      <address>
+        Rua Conde de Leopoldina, 276<br>
+        São Cristóvão - Rio de Janeiro - RJ<br>
+        CEP 20930-460<br>
+        <a href="tel:+552122014070">(21) 2201-4070</a>
+      </address>
+    </article>
+    <article class="m-4 border-t border-solid border-slate-800">
+      <h3 class="text-xl my-4 font-bold">Ponto2 . Bordado industrial</h3>
+      <p>Requisitos: Paulo Jorge<br>
+        Análise e desenvolvimento: <a href="mailto:anselmo@oxigenai.com.br">Anselmo</a></p>
+    </article>
   </div>
 </template>
