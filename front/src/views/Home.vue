23834991582dcd5090355da5be10af65f9ac9d79
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-24 16:55:56 -0300

    alterando logo e home

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center place-content-around">
-    <h1 class="text-8xl font-bold text-center text-cyan-950">RPR</h1>
-    <h1 class="text-8xl font-bold text-center text-cyan-950">Ponto2</h1>
-    <img src="../assets/logo.png" alt="Ponto2 logo" width="194" height="194">
+    <!-- <h1 class="text-8xl font-bold text-center text-cyan-950">RPR</h1> -->
+    <img src="../assets/logo.jpeg" alt="Ponto2 logo" width="171" height="188">
+    <!-- <h1 class="text-8xl font-bold text-center text-cyan-950">Ponto2</h1> -->
   </div>
 </template>
