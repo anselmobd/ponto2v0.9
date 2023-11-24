5a1cc84edc53bada180bc7dbb0a706057335c3ca
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-24 14:49:35 -0300

    salva user em cookie para recarregar em um reload de página

--- front/src/stores/auth.js
+++ front/src/stores/auth.js
@@ -1,10 +1,16 @@
 import { defineStore } from 'pinia'
+import VueCookies from 'vue-cookies'
+
+var saved_user = VueCookies.get('user');
+if (! saved_user) {
+  saved_user = {
+    name: ''
+  }
+}
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    user: {
-      name: ''
-    },
+    user: saved_user,
   }),
   actions: {
     setUser(name, access, refresh) {
@@ -13,11 +19,13 @@ export const useAuthStore = defineStore('auth', {
         access: access,
         refresh: refresh,
       };
+      VueCookies.set('user', this.user, "24h");
     },
     encerrar() {
       this.user = {
         name: '',
       };
+      VueCookies.remove('user');
     },
   }
 })
