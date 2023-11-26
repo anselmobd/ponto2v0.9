f90635d6ef46791f45b60a0d77630829a46a334a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-26 02:12:36 -0300

    ajusta nome de variável

--- front/src/services/auth-header.js
+++ front/src/services/auth-header.js
@@ -4,8 +4,8 @@ export default function authHeader() {
     const auth = useAuthStore()
     let user = auth.user;
   
-    if (user && user.accessToken) {
-      return { Authorization: 'Bearer ' + user.accessToken };
+    if (user && user.access) {
+      return { Authorization: 'Bearer ' + user.access };
     } else {
       return {};
     }
