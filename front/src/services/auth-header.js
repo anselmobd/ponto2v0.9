f3d859b91d890f86b09762a818191e02bd6dabb5
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-27 09:42:10 -0300

    code

--- front/src/services/auth-header.js
+++ front/src/services/auth-header.js
@@ -3,7 +3,7 @@ import { useAuthStore } from '../stores/auth.js';
 export function authHeader() {
   const auth = useAuthStore()
   if (auth?.user?.access) {
-    return { Authorization: 'Bearer ' + auth.user.access };
+    return { Authorization: `Bearer ${auth.user.access}` };
   }
   return {};
 }
