99c569f9ce26ec5a8e3dbaaf3786687e5af89f18
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-27 17:07:36 -0300

    layout

--- front/src/views/Bordado.vue
+++ front/src/views/Bordado.vue
@@ -2,7 +2,18 @@
 import { ref, onMounted } from 'vue'
 import { axiosPrivate } from '../common/axiosPrivate.js';
 
-const pedido_itens = ref(null)
+const pedido_itens = ref(null);
+const editing = ref(false);
+const cliente = ref({
+  input: '',
+  error: '',
+  list: []
+});
+const bordado = ref({
+  input: '',
+  error: '',
+  list: []
+});
 
 function getPedidoItens() {
   const params = new URLSearchParams();
@@ -16,7 +27,7 @@ function getPedidoItens() {
     pedido_itens.value = response.data.results;
   })
   .catch(error => {
-    console.log('error /bordado/api/pedido_item/', error)
+    console.log('Erro ao obter pedido_itens via API:', error)
   });
 }
 
@@ -41,6 +52,34 @@ onMounted(() => {
           <th>Bordado</th>
           <th>Ações</th>
         </tr>
+        <tr class="table__tr-input">
+          <th>-</th>
+          <th>
+            <span class="text-sm text-red-700 font-bold" v-if="cliente.error" >{{ cliente.error }}<br /></span>
+            <input
+              class="mx-0.5 border border-solid border-slate-500"
+              v-model.trim="cliente.input"
+              :disabled="!editing"
+              type="text"
+              ref="inputCliente"
+              placeholder="Cliente"
+            >
+          </th>
+          <th>
+            <span class="text-sm text-red-700 font-bold" v-if="bordado.error" >{{ bordado.error }}<br /></span>
+            <input
+              class="mx-0.5 border border-solid border-slate-500"
+              v-model.trim="bordado.input"
+              :disabled="!editing"
+              type="text"
+              ref="inputBordado"
+              placeholder="Bordado"
+            >
+          </th>
+          <th>
+            -
+          </th>
+        </tr>
       </thead>
       <tbody>
         <tr v-if="!pedido_itens">
@@ -54,8 +93,8 @@ onMounted(() => {
           <td>{{pedido_item.pedido.cliente.apelido}}</td>
           <td>{{pedido_item.bordado.nome}}</td>
           <td>
-            <button :disabled="editing" class="btn">Editar</button>
-            <button :disabled="editing" class="btn">Apagar</button>
+            <button :disabled="editing">Editar</button>
+            <button :disabled="editing">Apagar</button>
           </td>
         </tr>
       </tbody>
@@ -64,7 +103,16 @@ onMounted(() => {
 </template>
 
 <style scoped>
+.table__tr-input th {
+  @apply font-normal 
+}
 th, td {
   @apply border border-solid border-slate-300 text-center
+}
+button {
+  @apply mx-0.5 px-2 border border-solid border-slate-800 rounded-lg bg-cyan-600 font-bold text-slate-100
+}
+button:disabled {
+  @apply bg-slate-500
 }
 </style>
