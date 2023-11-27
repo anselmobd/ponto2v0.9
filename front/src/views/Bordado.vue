9d0ef04f46a6d2b20b220da13b966145655da541
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-27 14:32:14 -0300

    eliminando console.logs

--- front/src/views/Bordado.vue
+++ front/src/views/Bordado.vue
@@ -4,42 +4,24 @@ import { axiosPrivate } from '../common/axiosPrivate.js';
 
 const pedido_itens = ref(null)
 
-function getPedidoItens(tentativa = 1) {
-  console.log('getPedidoItens', tentativa);
-
+function getPedidoItens() {
   const params = new URLSearchParams();
   params.append('format', 'json');
-  console.log(params);
-
-  var result = 0;
 
   axiosPrivate.get(
     '/bordado/api/pedido_item/',
     {params: params}
   )
   .then(response => {
-    console.log('getPedidoItens then');
-    console.log(response);
-    console.log(response.data.results);
     pedido_itens.value = response.data.results;
-    result = response.status;
   })
   .catch(error => {
-    console.log('getPedidoItens catch');
-    console.log('Erro ao obter pedido_itens via API:', error);
-    console.log(error.response.status);
-    result = error.response.status;
-  })
-  .finally(function () {
-    console.log('getPedidoItens finally');
+    console.log('error /bordado/api/pedido_item/', error)
   });
-  console.log('getPedidoItens fim');
 }
 
 onMounted(() => {
-  console.log('onMounted');
   getPedidoItens();
-  console.log(pedido_itens);
 })
 </script>
 
