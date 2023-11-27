1a07ec2c149185145d7ab9e6610cee68061ebbdb
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-27 16:10:35 -0300

    iniciando formatação

--- front/src/views/Bordado.vue
+++ front/src/views/Bordado.vue
@@ -20,6 +20,11 @@ function getPedidoItens() {
   });
 }
 
+function pedidoItemInseridoEmData(pedido_item) {
+  var date = new Date(pedido_item.inserido_em)
+  return date.toLocaleDateString('pt-br') + ' ' + date.toLocaleTimeString('pt-br');
+}
+
 onMounted(() => {
   getPedidoItens();
 })
@@ -27,7 +32,39 @@ onMounted(() => {
 
 <template>
   <div>
-    <p>pedido_itens</p>
-    {{pedido_itens}}
+    <h4 class="text-xl text-center bg-slate-800 text-slate-100">Pedido</h4>
+    <table class="w-full">
+      <thead>
+        <tr>
+          <th>Data</th>
+          <th>Cliente</th>
+          <th>Bordado</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-if="!pedido_itens">
+          <td colspan="4">Carregando últimos pedidos...</td>
+        </tr>
+        <tr
+          v-for="pedido_item in pedido_itens"
+          :key="pedido_item.id"
+        >
+          <td>{{pedidoItemInseridoEmData(pedido_item)}}</td>
+          <td>{{pedido_item.pedido.cliente.apelido}}</td>
+          <td>{{pedido_item.bordado.nome}}</td>
+          <td>
+            <button :disabled="editing" class="btn">Editar</button>
+            <button :disabled="editing" class="btn">Apagar</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<style scoped>
+th, td {
+  @apply border border-solid border-slate-300 text-center
+}
+</style>
