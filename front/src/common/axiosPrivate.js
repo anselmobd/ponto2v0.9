c5f204e9c0061abbb13ea851d8364e50871404f7
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-27 15:24:58 -0300

    rename auth-header.js to authHeader.js

--- front/src/common/axiosPrivate.js
+++ front/src/common/axiosPrivate.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { mountAuthHeader } from '../services/auth-header.js'
+import { mountAuthHeader } from '../services/authHeader.js'
 import { refreshAccessToken } from '../services/refreshToken.js';
 
 
