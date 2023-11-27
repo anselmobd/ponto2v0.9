df8a7630492af0ad9509934e9c993e6f79bb78e4
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-27 15:44:29 -0300

    eliminando comments and console.logs

--- front/src/common/axiosPrivate.js
+++ front/src/common/axiosPrivate.js
@@ -12,8 +12,6 @@ export const axiosPrivate = axios.create({
 
 axiosPrivate.interceptors.request.use(
   async (config) => {
-    console.log('interceptors.request')
-    console.log('config', config)
     let auth_headers = mountAuthHeader();
     config.headers = {
       ...config.headers,
@@ -27,26 +25,13 @@ axiosPrivate.interceptors.request.use(
 axiosPrivate.interceptors.response.use(
   (response) => response,
   async (error) => {
-    console.log('interceptors.response error')
     const config = error?.config;
-    console.log('config', config)
-    console.log('error?.response?.status', error?.response?.status)
-    console.log('config?.sent', config?.sent)
 
     if (error?.response?.status === 401 && !config?.sent) {
-      console.log('interceptors.response entrou no if')
-
       config.sent = true;
 
-      // const result = await memoizedRefreshToken();
       const result = await refreshAccessToken();
-
-      // if (result?.accessToken) {
       if (result == 200) {
-        // config.headers = {
-        //   ...config.headers,
-        //   authorization: `Bearer ${result?.accessToken}`,
-        // };
         let auth_headers = mountAuthHeader();
         config.headers = {
           ...config.headers,
