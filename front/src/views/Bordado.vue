e79b32c1f0efd0e57a5cd1e31b7206351ea209ba
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-27 14:27:25 -0300

    eliminando comentários

--- front/src/views/Bordado.vue
+++ front/src/views/Bordado.vue
@@ -1,64 +1,11 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-// import { useAuthStore } from '../stores/auth.js';
-// import { axiosPublic } from '../common/axiosPublic.js';
 import { axiosPrivate } from '../common/axiosPrivate.js';
-import { refreshAccessToken } from '../common/refreshToken.js';
-// import { authHeader } from '../services/auth-header.js'
 
 const pedido_itens = ref(null)
 
-// function refreshToken(execFunction = null, param = null) {
-//   console.log('refreshToken');
-//   const auth = useAuthStore()
-
-//   const params = new URLSearchParams();
-//   params.append('format', 'json');
-//   console.log(params);
-
-//   const { setUser } = auth
-
-//   var result = 0;
-
-//   axiosPublic.post(
-//     '/api/token/refresh/',
-//     {refresh: auth.user.refresh},
-//     {params: params} // , headers: headers}
-//   )
-//   .then(response => {
-//     console.log('refreshToken then');
-//     console.log(response.status);
-//     console.log(response.data);
-//     if (response.data.access) {
-//       setUser(
-//         auth.user.name,
-//         response.data.access,
-//         auth.user.refresh
-//       );
-//     }
-//     result = response.status;
-//   })
-//   .catch(error => {
-//     console.log('refreshToken catch');
-//     console.log(error.status);
-//     console.log('Erro ao dar refresh na access key:', error);
-//     console.log(error.response.status);
-//     result = error.response.status;
-//   })
-//   .finally(function () {
-//     console.log('refreshToken finally');
-//     if (result == 200 && execFunction != null) {
-//       execFunction(param);
-//     }
-//   });
-//   console.log('refreshToken fim');
-// }
-
 function getPedidoItens(tentativa = 1) {
   console.log('getPedidoItens', tentativa);
-
-  // let headers = authHeader();
-  // console.log(headers);
 
   const params = new URLSearchParams();
   params.append('format', 'json');
@@ -68,7 +15,7 @@ function getPedidoItens(tentativa = 1) {
 
   axiosPrivate.get(
     '/bordado/api/pedido_item/',
-    {params: params} // , headers: headers}
+    {params: params}
   )
   .then(response => {
     console.log('getPedidoItens then');
@@ -85,31 +32,6 @@ function getPedidoItens(tentativa = 1) {
   })
   .finally(function () {
     console.log('getPedidoItens finally');
-    // if (result == '4401' && tentativa == 1) {
-    //   console.log('status == 401');
-    //   console.log('call refreshAccessToken');
-
-    //   refreshAccessToken()
-    //   .then(refreshStatusResponse => {
-    //     console.log('refreshAccessToken then')
-    //     console.log('refreshStatusResponse', refreshStatusResponse);
-    //     if (refreshStatusResponse == 200) {
-    //       console.log('entrou no if do then');
-    //       getPedidoItens(2);
-    //     } else {
-    //       console.log('Não entrou no if do then');
-    //     }
-    //   });
-
-    //   // const refreshStatus = refreshAccessToken();      
-    //   // console.log('refreshStatus', refreshStatus);
-    //   // if (refreshStatus == 200) {
-    //   //   console.log('entrou no if refreshStatus == 200');
-    //   //   getPedidoItens(2);
-    //   // } else {
-    //   //   console.log('Não entrou no if refreshStatus == 200');
-    //   // }
-    // }
   });
   console.log('getPedidoItens fim');
 }
