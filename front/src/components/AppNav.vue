10ecce0973ff908185c37eba07c0756ffd92e39d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-28 18:01:14 -0300

    ajustando estilos dos botões

--- front/src/components/AppNav.vue
+++ front/src/components/AppNav.vue
@@ -29,10 +29,14 @@ const { encerrar } = auth
 
     <div>
       <span v-if="user.name">{{user.name}}&ThickSpace;</span>
-      <router-link v-if="!user.name" :to="{ name: 'login' }" class="px-2 py-0.5 border border-solid border-slate-800 rounded-lg bg-sky-600 font-bold text-slate-100">Indentificar-se</router-link>
+      <router-link
+        class="px-2 py-0.5 rounded-lg bg-sky-700 font-bold text-slate-100"
+        v-if="!user.name"
+        :to="{ name: 'login' }"
+      >Indentificar-se</router-link>
       <button
         type="button"
-        class="px-2 border border-solid border-slate-800 rounded-lg bg-sky-600 font-bold text-slate-100"
+        class="px-2 py-0.5 rounded-lg bg-sky-700 font-bold text-slate-100"
         v-if="user.name" 
         @click.stop="encerrar(); router.push({ name: 'home' })"
       >Encerrar</button>
