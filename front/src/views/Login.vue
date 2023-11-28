9b1c7b0316e257654aa6b517d86c4ccc648ea281
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-28 17:56:17 -0300

    passando tela de login para tailwindcss

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -40,12 +40,12 @@ function login() {
 
 <template>
   <div>
-    <h2 class="login__h2">Identificação</h2>
-    <form @submit.prevent="login()" class="login__form">
-      <p class="login__p">
-        <label class="login__label" for="username">Usuário:</label>
+    <h2 class="font-bold text-lg">Identificação</h2>
+    <form @submit.prevent="login()">
+      <p class="my-4">
+        <label class="block" for="username">Usuário:</label>
         <input
-          class="login__input"
+          class="px-2 py-1 w-full border-2 rounded-xl"
           type="text"
           name="username"
           id="username"
@@ -55,10 +55,10 @@ function login() {
           @input="alerta = ''"
           required>
       </p>
-      <p class="login__p">
-        <label class="login__label" for="password">Senha:</label>
+      <p class="my-4">
+        <label class="block" for="password">Senha:</label>
         <input
-          class="login__input"
+          class="px-2 py-1 w-full border-2 rounded-xl"
           type="password"
           name="password"
           id="password"
@@ -67,43 +67,11 @@ function login() {
           @input="alerta = ''"
           required>
       </p>
-      <p v-if="alerta" class="text-red-600">{{ alerta }}</p>
-      <button class="login__button" type="submit">Enviar</button>
+      <p v-if="alerta" class="my-4 text-red-600">{{ alerta }}</p>
+      <button
+        class="px-2 py-1 rounded-xl bg-sky-700 font-bold text-slate-100"
+        type="submit"
+      >Enviar</button>
     </form>
   </div>
 </template>
-
-<style scoped>
-/* || CONTACT */
-
-.login__h2 {
-    margin: 0;
-    font-weight: bold;
-    font-size: larger;
-}
-
-.login__p {
-    margin: 1em 0;
-}
-
-.login__label {
-    display: block;
-    font-weight: bold;
-}
-
-.login__input,
-.login__textarea {
-    padding: 0.5em;
-    border-radius: 15px;
-    border-width: 2px;
-    width: 100%;
-}
-
-.login__button {
-    padding: 0.5em;
-    border-radius: 15px;
-    background-color: rgb(52, 136, 161);
-    color: whitesmoke;
-    font-weight: bold;
-}
-</style>
