4f13f0517b1fc6dc5d689d1538833d046a156f86
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-29 19:38:30 -0300

    comentários

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="flex flex-col items-center place-content-around">
-    <!-- <h1 class="text-8xl font-bold text-center text-sky-950">RPR</h1> -->
-    <img src="../assets/logo.jpeg" alt="Ponto2 logo" width="171" height="188">
-    <!-- <h1 class="text-8xl font-bold text-center text-sky-950">Ponto2</h1> -->
+    <img src="../assets/logo.jpeg" alt="Imagem representando RPR" width="171" height="188">
   </div>
 </template>
