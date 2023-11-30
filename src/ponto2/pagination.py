2247afe4741ff8ab2c958de541769e1569e1ad52
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-30 12:28:17 -0300

    pagination agora retorna mais informações

--- src/ponto2/pagination.py
+++ src/ponto2/pagination.py
@@ -1,5 +1,24 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import (
+    PageNumberPagination,
+    Response,
+)
 
 
 class CustomPageNumberPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        try:
+            previous = self.page.previous_page_number()
+        except Exception:
+            previous = None
+        return Response({
+            'count': self.page.paginator.count,
+            'per_page': self.page.paginator.per_page,
+            'page': self.page.number,
+            'next_link': self.get_next_link(),
+            'previous_link': self.get_previous_link(),
+            'next': self.page.next_page_number(),
+            'previous': previous,
+            'results': data,
+        })
