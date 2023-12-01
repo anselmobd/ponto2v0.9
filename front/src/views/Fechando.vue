8e4b465434aaf7df44d67947d9fd2d695b0ea3b6
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-11-30 23:02:10 -0300

    chamando getPedidoItem

--- front/src/views/Fechando.vue
+++ front/src/views/Fechando.vue
@@ -1,7 +1,32 @@
 <script setup>
 import { useRoute } from "vue-router";
+import { onMounted } from 'vue'
+import { getPedidoItem } from '../api/pedidoItem.js';
 
 const route = useRoute();
+
+// DB API calls (do) and callbacks (cb)
+
+function cbPedidoItem(data, error) {
+  if (data) {
+    console.log(data);
+    // if (data?.results) pedido_itens.value = data.results;
+  }
+}
+
+function doGetPedidoItem() {
+  getPedidoItem({
+    id: route.params.id,
+    callBack: cbPedidoItem
+  });
+}
+
+// Lifecycle Hooks
+
+onMounted(() => {
+  doGetPedidoItem();
+})
+
 </script>
 
 <template>
