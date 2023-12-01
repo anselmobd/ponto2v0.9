27e0f79f4f20828fe11be70f85213ab4169896b9
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-12-01 17:50:05 -0300

    criada função date2InputText

--- front/src/utils/date.js
+++ front/src/utils/date.js
@@ -10,3 +10,10 @@ export function dateTime2Text(date_time) {
   const time = date_time.toLocaleTimeString('pt-br');
   return date + ' ' + time;
 }
+
+export function date2InputText(date) {  
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  return `${year}-${month}-${day}`
+}
