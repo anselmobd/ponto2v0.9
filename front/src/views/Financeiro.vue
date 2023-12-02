1ea07261189ca787406bcca2130deb74c807d6ce
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-12-02 11:18:02 -0300

    implementando botão Voltar

--- front/src/views/Financeiro.vue
+++ front/src/views/Financeiro.vue
@@ -1,4 +1,5 @@
 <script setup>
+import router from '@/router'
 import { useRoute } from "vue-router";
 import { ref, onMounted, watch } from 'vue'
 
@@ -8,6 +9,18 @@ const route = useRoute();
 
 <template>
   <div>
-    <h2 class="my-4 font-bold text-xl">Financeiro do pedido <span class="text-indigo-700">{{ route.params.id }}</span></h2>
+    <div class="flex my-4 place-content-between">
+      <h2 class="inline font-bold text-xl">Financeiro do pedido <span class="text-indigo-700">{{ route.params.id }}</span></h2>
+      <a title="Voltar" class="button text-xl cursor-pointer" @click.prevent="router.go(-1)">&#x2190;</a>
+    </div>
   </div>
 </template>
+
+<style scoped>
+button, .button {
+  @apply mx-0.5 my-[1px] px-2 py-0.5 rounded-lg bg-sky-700 font-bold text-slate-100
+}
+button:disabled {
+  @apply bg-slate-500
+}
+</style>
