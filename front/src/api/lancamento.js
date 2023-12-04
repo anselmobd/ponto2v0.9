e571fe3f1c5f48aee769d28769d1ba58730eb754
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-12-04 08:40:58 -0300

    iniciando getLancamentos

---
+++ front/src/api/lancamento.js
@@ -0,0 +1,58 @@
+import { axiosPrivate } from '../common/axiosPrivate.js';
+
+export function getLancamentos({
+  page=1,
+  cliente_apelido=null,
+  callBack=()=>{}
+}) {
+  const params = new URLSearchParams();
+  params.append('format', 'json');
+  if (page && page >1) {
+    params.append('page', page);
+  }
+  if (cliente_apelido) {
+    params.append('cliente__apelido', cliente_apelido);
+  }
+  axiosPrivate.get(
+    '/bordado/api/lancamento/',
+    {params: params}
+  )
+  .then(response => {
+    callBack(response.data);
+  })
+  .catch(error => {
+    console.error('Erro ao obter lançamentos via API:', error)
+    callBack(null, error);
+  });
+}
+
+export function addLancamento({
+  payload={
+    "cliente": {
+      "apelido": null,
+    },
+    "tipo": null,
+    "nf": null,
+    "valor": null,
+    "data": null,
+    "parcelamento": null,
+    "pedidos_itens": []
+  },  
+  callBack=()=>{}
+}) {
+  const params = new URLSearchParams();
+  params.append('format', 'json');
+  console.log('addCobranca', payload);
+  axiosPrivate.post(
+    `/bordado/api/lancamento/`,
+    payload,
+    {params: params}
+  )
+  .then(response => {
+    callBack(response.data);
+  })
+  .catch(error => {
+    console.error('Erro ao adicionar lancamento via API:', error)
+    callBack(null, error);
+  });
+}
