357b1ebcc5cb955ecec60fa0dba3aab3f168175d
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2023-12-04 13:48:28 -0300

    bug na lógica do disabled

--- front/src/views/Financeiro.vue
+++ front/src/views/Financeiro.vue
@@ -313,7 +313,7 @@ onMounted(() => {
 
     <section id="insere_comunicado">
       <button
-        :disabled="!pedidos_selecionados.length || status != 'b'"
+        :disabled="(!pedidos_selecionados.length) || (status != 'b')"
         class="px-2 py-1 rounded-xl bg-sky-700 font-bold text-slate-100"
         @click="handleInserirComunicadoClick"
       >Comunicar cobrança</button>
